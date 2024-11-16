1140a0bf52190aaa7a506f741c82b9b109d4459d
Bruno Castillo <castillobrh@gmail.com>
2024-11-16 11:57:04 -0300

    fix migrations snapshot

--- Proyecto_trivia_BED/Migrations/TriviaContextModelSnapshot.cs
+++ Proyecto_trivia_BED/Migrations/TriviaContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace Proyecto_trivia_BED.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
+                .HasAnnotation("Relational:MaxIdentifierLength", 128) 
                 .HasAnnotation("ProductVersion", "5.0.17")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
